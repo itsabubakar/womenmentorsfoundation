c31b9734b2c454fc6208b2b262d1e2654ee07fe5
Sadiq <seeker99157@gmail.com>
2022-10-31 12:51:21 +0100

    changed event date

--- components/Events.jsx
+++ components/Events.jsx
@@ -11,7 +11,7 @@ const Events = () => {
                 <div className="mt-5 xl:ml-10 w-60 sm:w-full">
                     <Link href={'/articles/awards'}><h2 className="mb-6 xl:mb-0 cursor-pointer underline decoration-Moody-blue underline-offset-[9px] text-center text-2xl text-gray-800 hover:decoration-Sail">AWARD CEREMONY FOR WOMEN POLITICAL ASPIRANTS</h2></Link>
 
-                    <p className="mb-2 text-sm text-gray-700 text-center xl:text-left">26th November, 2022 | 10AM | Unity Hall, Halal Hotel</p>
+                    <p className="mb-2 text-sm text-gray-700 text-center xl:text-left">30th November, 2022 | 10AM | Unity Hall, Halal Hotel</p>
                     <p className="mb-2 text-gray-800 text-center xl:text-left">At the event, special awards will be given to deserving female political aspirants who contested in the last primary elections across all political parties within Kaduna state irrespective of their political party affiliations.</p>
                 </div>
             </div>
